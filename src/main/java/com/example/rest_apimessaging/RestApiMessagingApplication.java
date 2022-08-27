df85751a4f20932e94e9dc594f2f46367733be47
Kisalay <kisalaykisu@gmail.com>
2022-08-27 23:35:52 +0530

    first commit

---
+++ src/main/java/com/example/rest_apimessaging/RestApiMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_apimessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiMessagingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rest_apimessaging/RestApiMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_apimessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMessagingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
