17037b138712f89c2844f8914d1d8ff880a81d84
Kisalay <kisalaykisu@gmail.com>
2022-08-27 23:57:55 +0530

    UC2 has been added:

---
+++ src/main/java/com/example/rest_apimessaging/controller/RestAPI.java
@@ -0,0 +1,19 @@
+package com.example.rest_apimessaging.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class RestAPI {
+    @RequestMapping(value = {"","/","/home"}, method = RequestMethod.GET)
+    public String welcome(){
+        return "<h1>Hello From BridgeLabz!!</h1>";
+    }
+
+    //Use GET Request Method and pass name as query parameter
+    @GetMapping("/query")
+    public String getUsers(@RequestParam(value = "name", defaultValue = "Santhosh") String name)
+    {
+        return "Hello! " + name + " From BridgeLabz!!";
+    }
+}
